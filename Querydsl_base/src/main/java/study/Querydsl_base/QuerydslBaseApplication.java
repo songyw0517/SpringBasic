9882a951b24d023e5b7b9b504b103bb9668c82d9
Scof <32575460+songyw0517@users.noreply.github.com>
2022-04-05 02:10:11 +0900

    create Querydsl base

---
+++ Querydsl_base/src/main/java/study/Querydsl_base/QuerydslBaseApplication.java
@@ -0,0 +1,13 @@
+package study.Querydsl_base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslBaseApplication.class, args);
+	}
+
+}
---
+++ Querydsl_base/src/test/java/study/Querydsl_base/QuerydslBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package study.Querydsl_base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
