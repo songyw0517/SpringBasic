865426ac9b45fa84a74dbfd81b9d82c325cd6236
Scof <32575460+songyw0517@users.noreply.github.com>
2022-04-04 20:56:10 +0900

    create Spring_Mysql_Connect

---
+++ SpringMysqlConnect/src/main/java/spring/mysql/SpringMysqlConnect/SpringMysqlConnectApplication.java
@@ -0,0 +1,13 @@
+package spring.mysql.SpringMysqlConnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMysqlConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMysqlConnectApplication.class, args);
+	}
+
+}
---
+++ SpringMysqlConnect/src/test/java/spring/mysql/SpringMysqlConnect/SpringMysqlConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.mysql.SpringMysqlConnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMysqlConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
