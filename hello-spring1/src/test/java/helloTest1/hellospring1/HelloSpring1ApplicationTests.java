02e3649639e9be1369ade7cbd82240ecf5583174
Scof <32575460+songyw0517@users.noreply.github.com>
2022-04-22 06:41:04 +0900

    new spring

---
+++ hello-spring1/src/test/java/helloTest1/hellospring1/HelloSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package helloTest1.hellospring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
