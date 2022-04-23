62b3bed1047b4fb887c4b3f57662524651ced72e
Scof <32575460+songyw0517@users.noreply.github.com>
2022-04-23 13:45:31 +0900

    member 분리

---
+++ MemberReview/src/main/java/hello/MemberReview/MemberReviewApplication.java
@@ -0,0 +1,13 @@
+package hello.MemberReview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MemberReviewApplication.class, args);
+	}
+
+}
---
+++ MemberReview/src/test/java/hello/MemberReview/MemberReviewApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.MemberReview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberReviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
